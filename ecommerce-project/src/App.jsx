f0a8c8ee477170581f64a83c6c4f9ca5586af4a4
skynet <skynetdigitalsolutionsug@gmail.com>
2026-05-17 14:32:39 +0300

    Pages Transferred to React App

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/Homepage.jsx'
+import { CheckOut } from './pages/CheckoutPage.jsx'
 import './App.css'
 
 function App() {
   return (
-    <HomePage />
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckOut />} />
+    </Routes>
   )
 }
 
