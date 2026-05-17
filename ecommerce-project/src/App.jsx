6168d9cfb2b3e936a343271a6c22824bc61010a1
skynet <skynetdigitalsolutionsug@gmail.com>
2026-05-17 14:34:14 +0300

    Pages Transfered to React App

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/Homepage.jsx'
 import { CheckOut } from './pages/CheckoutPage.jsx'
+import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage.jsx' 
 import './App.css'
 
 function App() {
   return (
     <Routes>
-      <Route index element={<HomePage />} />
+      <Route path='/' element={<HomePage />} />
       <Route path="checkout" element={<CheckOut />} />
+      <Route path = 'orders' element = {<OrdersPage/>} />
+      <Route path='tracking' element = {<TrackingPage/>} />
     </Routes>
   )
 }
--- ecommerce-project/src/pages/CheckoutPage.jsx
+++ ecommerce-project/src/pages/CheckoutPage.jsx
@@ -1,3 +1,6 @@
+import './checkout-header.css'
+import './CheckoutPage.css'
+
 export function CheckOut() {
     return (
         <>
@@ -5,7 +8,7 @@ export function CheckOut() {
             <div className="checkout-header">
                 <div className="header-content">
                     <div className="checkout-header-left-section">
-                        <a href="index.html">
+                        <a href="/">
                             <img className="logo" src="images/logo.png" />
                             <img className="mobile-logo" src="images/mobile-logo.png" />
                         </a>
@@ -13,7 +16,7 @@ export function CheckOut() {
 
                     <div className="checkout-header-middle-section">
                         Checkout (<a className="return-to-home-link"
-                            href="index.html">3 items</a>)
+                            href="/">3 items</a>)
                     </div>
 
                     <div className="checkout-header-right-section">
