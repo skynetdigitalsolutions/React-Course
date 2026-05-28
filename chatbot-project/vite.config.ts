06e36b548ce4706aa4a02a0770ba39df913f1f24
skynetdigitalsolutions <skynetdigitalsolutionsug@gmail.com>
2026-05-28 20:44:20 +0300

    Fixing the Chatbot Project

--- chatbot-project/vite.config.ts
+++ chatbot-project/vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ReactCompiler from 'babel-plugin-react-compiler'
 
 // https://vite.dev/config/
-
 export default defineConfig({
-  plugins: [
-    react(),
-    ReactCompiler({
-      target: '18' // Configured directly here safely
-    }),
-  ],
+  plugins: [react({
+    babel: {
+      plugins: [['babel-plugin-react-compiler', { target: '18' }]],
+    },
+  })],
 })
-
